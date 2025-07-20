c65c5ba8453eeca47d462dd25af63c29680e8ad1
Joydipta Chanda <joydiptajdc@gmail.com>
2025-07-20 19:38:24 +0530

    Added basic signal handling for SIGINT and SIGTERM

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
+#include <csignal>
+#include <ncurses.h>
+#include <iostream>
 #include "TetrisGame.hpp"
+
+void handle_signal(int sig) {
+    endwin();
+    Logger::getInstance().log("\nTetris closed by signal " + sig);
+    std::exit(1);
+}
 
 int main()
 {
+    std::signal(SIGINT, handle_signal);  // Ctrl-C
+    std::signal(SIGTERM, handle_signal); // kill command
     TetrisGame game;
     game.run();
     return 0;
