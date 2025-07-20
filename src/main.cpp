7845a82233befe03bd093af68e4519d9c49bc1df
Joydipta Chanda <joydiptajdc@gmail.com>
2025-07-20 20:35:01 +0530

    Added resize signal handling and interface redraw.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include <csignal>
 #include <ncurses.h>
 #include <iostream>
+#include <string>
 #include "TetrisGame.hpp"
+#include "Logger.hpp"
 
-void handle_signal(int sig) {
+// Graceful shutdown on Ctrl+C or kill
+void handle_signal(int sig)
+{
     endwin();
-    Logger::getInstance().log("\nTetris closed by signal " + sig);
+    Logger::getInstance().log("Tetris closed by signal " + std::to_string(sig));
+    std::cerr << "\nTetris closed by signal " << sig << ".\n";
     std::exit(1);
+}
+
+// Terminal resize handler
+void handle_winch(int sig)
+{
+    // Let ncurses know window size may have changed
+    endwin();
+    refresh();
+    clear();
+    // NOTE: The next game loop iteration will redraw everything.
+    Logger::getInstance().log("Terminal resized (SIGWINCH received)");
 }
 
 int main()
 {
     std::signal(SIGINT, handle_signal);  // Ctrl-C
     std::signal(SIGTERM, handle_signal); // kill command
+    std::signal(SIGWINCH, handle_winch); // terminal resize
+
     TetrisGame game;
     game.run();
     return 0;
